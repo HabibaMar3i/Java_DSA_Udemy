8f16ee08e379fc43ab934ef88830ded0fe0d4f09
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-09-13 20:53:58 +0300

    added O(n*2 + n) which is drop non dominant

--- BigO.java
+++ BigO.java
@@ -17,17 +17,30 @@ public class BigO {
     // }
 
     // O(n*n) -> O(n*2)
+    // public static void printNums(int n){
+    //     for(int i = 0; i < n; i++){
+    //         for(int j = 0; j < n; j++){
+    //         System.out.println(i + " " + j);
+    //         }
+    //     }
+    // }
+
+    // O(n*2 + n) we drop non-dominant so it will be O(n*2)
     public static void printNums(int n){
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
             System.out.println(i + " " + j);
             }
         }
+        for(int i = 0; i < n; i++){
+            System.out.println(i);
+        }
     }
 
     public static void main(String[] args) {
         // printNums(10); // O(n) gave 9 numbers printed 9 
         // printNums(10); // O(n) gave 9 numbers printed same twice
-        printNums(10); // O(n*2) -> 9*9 
+        // printNums(10); // O(n*2) -> 9*9 
+        printNums(10); // O(n*2 + n) -> ((9*9) + 9)
     }
 }
