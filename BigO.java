2a50775b92d1e2f25f41d94623b1183bd98ec445
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-09-13 20:48:48 +0300

    added O(n+n) or O(n)

--- BigO.java
+++ BigO.java
@@ -1,12 +1,23 @@
 public class BigO {
+    // O(n)
+    // public static void printNums(int n){
+    //     for(int i = 0; i < n; i++){
+    //         System.out.println(i);
+    //     }
+    // }
+
+    // O(n+n) -> O(2n) remove constant so it's O(n)
     public static void printNums(int n){
+        for(int i = 0; i < n; i++){
+            System.out.println(i);
+        }
         for(int i = 0; i < n; i++){
             System.out.println(i);
         }
     }
 
     public static void main(String[] args) {
-        printNums(10); // O(n) gave 9 numbers printed 9 
-
+        // printNums(10); // O(n) gave 9 numbers printed 9 
+        printNums(10); // O(n) gave 9 numbers printed same twice
     }
 }
