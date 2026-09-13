4b89de60b4e8d2fc05f6b771b3826a0690391e8f
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-09-13 20:51:31 +0300

    added O(n*2)

--- BigO.java
+++ BigO.java
@@ -7,17 +7,27 @@ public class BigO {
     // }
 
     // O(n+n) -> O(2n) remove constant so it's O(n)
+    // public static void printNums(int n){
+    //     for(int i = 0; i < n; i++){
+    //         System.out.println(i);
+    //     }
+    //     for(int i = 0; i < n; i++){
+    //         System.out.println(i);
+    //     }
+    // }
+
+    // O(n*n) -> O(n*2)
     public static void printNums(int n){
         for(int i = 0; i < n; i++){
-            System.out.println(i);
-        }
-        for(int i = 0; i < n; i++){
-            System.out.println(i);
+            for(int j = 0; j < n; j++){
+            System.out.println(i + " " + j);
+            }
         }
     }
 
     public static void main(String[] args) {
         // printNums(10); // O(n) gave 9 numbers printed 9 
-        printNums(10); // O(n) gave 9 numbers printed same twice
+        // printNums(10); // O(n) gave 9 numbers printed same twice
+        printNums(10); // O(n*2) -> 9*9 
     }
 }
